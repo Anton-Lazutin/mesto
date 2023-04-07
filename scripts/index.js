b08d5ee64f64af9792d860425d05d6728bb3b5f9
Anton Lazutin <laa.63@yandex.ru>
2023-04-07 21:21:41 +0300

    bugs fixed

--- scripts/index.js
+++ scripts/index.js
@@ -2,30 +2,29 @@ let openButton = document.querySelector('.profile__edit-btn');
 let popup = document.querySelector('.popup');
 
 function openPopup() {
-    popup.classList.add('popup__opened');
+    popup.classList.add('popup_opened');
 }
 openButton.addEventListener('click', openPopup);
 
 let closeButton = document.querySelector('.popup__close-btn');
 
 function closePopup() {
-    popup.classList.remove('popup__opened');
+    popup.classList.remove('popup_opened');
 }
 closeButton.addEventListener('click', closePopup);
 
 let profileName = document.querySelector('.profile__name');
 let profileHobby = document.querySelector('.profile__hobby');
-let inputName = document.querySelector('.popup__name');
-let inputHobby = document.querySelector('.popup__hobby');
+let inputName = document.querySelector('.popup__input_name');
+let inputHobby = document.querySelector('.popup__input_hobby');
 
 inputName.value = profileName.textContent;
 inputHobby.value = profileHobby.textContent;
 
-let buttonSubmit = document.querySelector('.popup__submit-btn');
 function handleFormSubmit(evt) {
     evt.preventDefault();
-    inputName = document.querySelector('popup__name');
-    inputHobby = document.querySelector('.popup__hobby');
+    inputName = document.querySelector('popup__input_name');
+    inputHobby = document.querySelector('.popup__input_hobby');
     profileName.textContent = inputName.value;
     profileHobby.textContent = inputHobby.value;
 }
