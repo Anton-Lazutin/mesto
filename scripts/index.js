b04b58f602d31e8c1af6c45c76c1355faaabe6a1
Anton Lazutin <laa.63@yandex.ru>
2023-04-11 09:55:01 +0300

    bugs fixed

--- scripts/index.js
+++ scripts/index.js
@@ -18,15 +18,13 @@ closeButton.addEventListener('click', closePopup);
 
 let profileName = document.querySelector('.profile__name');
 let profileHobby = document.querySelector('.profile__hobby');
-let inputName = document.querySelector('.popup__input-name');
-let inputHobby = document.querySelector('.popup__input-hobby');
+let inputName = document.querySelector('.popup__input-form_name');
+let inputHobby = document.querySelector('.popup__input-form_hobby');
 
 let formElement = document.querySelector('.popup__input-form');
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
-    inputName = document.querySelector('.popup__input-name');
-    inputHobby = document.querySelector('.popup__input-hobby');
     profileName.textContent = inputName.value;
     profileHobby.textContent = inputHobby.value;
     closePopup();
